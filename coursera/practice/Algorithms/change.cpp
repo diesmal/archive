7351d41c892399dede17ecaa08a00d0668d7546f
inikolaenko <ilia.nikolaenko@virtual-solution.com>
2018-08-20 17:28:17 +0200

    removed unused stuff

--- coursera/practice/Algorithms/change.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-    long m;
-    std::cin >> m;
-    std::cout <<  ((m / 10) + ((m % 10) / 5) + (m % 10) % 5) << '\n';
-}
-
--- scripts/rename.sh
+++
@@ -1,5 +0,0 @@
-j=0;
-for i in *.jpg
-do let j+=1;
-mv $i planet_animation_frame_$j@3x.jpg ;
-done
